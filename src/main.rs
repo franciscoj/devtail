25daff82e788cc8600b8879a543b207985dc5e48
Fran Casas <nflamel@gmail.com>
2020-04-19 14:31:32 +0200

    refactor: implement Iterator

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::io::prelude::*;
 use std::io::{BufReader, SeekFrom};
 
 struct Tail {
-    reader: BufReader<File>
+    reader: BufReader<File>,
 }
 
 impl Tail {
@@ -17,26 +17,34 @@ impl Tail {
     }
 }
 
-fn main() {
-    let file_name = env::args().nth(1).unwrap();
-    let tail = Tail::new(file_name.clone());
-    let mut reader = tail.reader;
+impl Iterator for Tail {
+    type Item = String;
 
-    loop {
+    fn next(&mut self) -> Option<Self::Item> {
         let mut line = String::new();
-        let res = reader.read_line(&mut line);
+        let mut maybe_line = None;
 
-        match res {
-            Ok(len) => {
-                if len > 0 {
-                    println!("=> {}", line.replace("\n", ""));
+        while let None = maybe_line {
+            if let Ok(len) = self.reader.read_line(&mut line) {
+                maybe_line = if len > 0 {
+                    Some(line.clone().replace("\n", ""))
+                } else {
+                    None
                 }
-            }
-
-            Err(err) => {
-                println!("=> ERR: {}", err)
+            } else {
+                // TODO: This can be handled, might mean that the file has been truncated or
+                // something like that.
+                panic!("Error reading the file!")
             }
         }
 
+        maybe_line
     }
+}
+
+fn main() {
+    let file_name = env::args().nth(1).unwrap();
+    let tail = Tail::new(file_name.clone());
+
+    tail.for_each(|line| println!("=> {}", line))
 }
