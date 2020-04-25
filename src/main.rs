49852a33218c5f59ccc8cc4297a9717476e19b56
Fran Casas <nflamel@gmail.com>
2020-04-25 22:33:36 +0200

    refactor: move run to Tail

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,7 @@
 extern crate clap;
 
 use clap::Arg;
-use devtail::tail::{Tail, TailF, TailS};
-use regex::Regex;
-use std::collections::hash_map::Entry;
-use std::collections::HashMap;
-use std::io::{self, BufRead};
-
-// Got this from here: https://stackoverflow.com/a/6640851/233720
-const UUID_REGEX: &str =
-    r"\[(\b[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12}\b)\].*";
+use devtail::tail::{TailF, TailS};
 
 fn main() {
     let matches = app_from_crate!()
@@ -25,58 +17,11 @@ fn main() {
     if let Some(file_name) = matches.value_of("file") {
         let tail = TailF::new(file_name.to_string());
 
-        run(tail)
+        tail.run()
     } else {
-        let stdin = io::stdin();
+        let stdin = std::io::stdin();
         let tail = TailS::new(&stdin);
 
-        run(tail)
-    }
-}
-
-fn run<T: BufRead>(tail: Tail<T>) {
-    let regex = Regex::new(UUID_REGEX).unwrap();
-    let mut map: HashMap<String, bool> = HashMap::new();
-
-    tail.for_each(|line| {
-        let maybe_capures = regex.captures(&line);
-
-        if let Some(captures) = maybe_capures {
-            let entry = map.entry(captures.get(1).unwrap().as_str().to_string());
-
-            if let Entry::Vacant(o) = entry {
-                o.insert(true);
-                println!("=> {}", line);
-            };
-        };
-    });
-}
-
-#[cfg(test)]
-mod tests {
-    use regex::Regex;
-
-    #[test]
-    fn regex_matches_uuid() {
-        let line = r#"[df7f9091-18d5-4002-91c9-e084516526ab] Started POST "/visits" for 127.0.0.1 at 2020-04-18 17:50:07 +0200"#;
-        let regex = Regex::new(crate::UUID_REGEX).unwrap();
-        let captures = regex.captures(&line).unwrap();
-
-        assert_eq!(
-            captures.get(1).unwrap().as_str(),
-            "df7f9091-18d5-4002-91c9-e084516526ab"
-        );
-    }
-
-    #[test]
-    fn regex_matches_uuid_on_line_with_brackets() {
-        let line = r#"[be155bd9-587d-468a-994f-441815edc79d]   CACHE MyModel Load (0.0ms)  SELECT  `my_models`.* FROM `my_models` WHERE `my_models`.`id` = 1 LIMIT 1  [["id", 1], ["LIMIT", 1]]"#;
-        let regex = Regex::new(crate::UUID_REGEX).unwrap();
-        let captures = regex.captures(&line).unwrap();
-
-        assert_eq!(
-            captures.get(1).unwrap().as_str(),
-            "be155bd9-587d-468a-994f-441815edc79d"
-        );
+        tail.run()
     }
 }
--- src/tail.rs
+++ src/tail.rs
@@ -1,7 +1,14 @@
+use regex::Regex;
+use std::collections::hash_map::Entry;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::{BufRead, BufReader, SeekFrom};
 use std::io::{Stdin, StdinLock};
+
+// Got this from here: https://stackoverflow.com/a/6640851/233720
+const UUID_REGEX: &str =
+    r"\[(\b[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12}\b)\].*";
 
 pub struct Tail<T: BufRead> {
     reader: T,
@@ -28,6 +35,26 @@ impl<'a> TailS<'a> {
     }
 }
 
+impl<T: BufRead> Tail<T> {
+    pub fn run(self) {
+        let regex = Regex::new(UUID_REGEX).unwrap();
+        let mut map: HashMap<String, bool> = HashMap::new();
+
+        self.for_each(|line| {
+            let maybe_capures = regex.captures(&line);
+
+            if let Some(captures) = maybe_capures {
+                let entry = map.entry(captures.get(1).unwrap().as_str().to_string());
+
+                if let Entry::Vacant(o) = entry {
+                    o.insert(true);
+                    println!("=> {}", line);
+                };
+            };
+        });
+    }
+}
+
 impl<T: BufRead> Iterator for Tail<T> {
     type Item = String;
 
@@ -50,5 +77,34 @@ impl<T: BufRead> Iterator for Tail<T> {
         }
 
         maybe_line
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use regex::Regex;
+
+    #[test]
+    fn regex_matches_uuid() {
+        let line = r#"[df7f9091-18d5-4002-91c9-e084516526ab] Started POST "/visits" for 127.0.0.1 at 2020-04-18 17:50:07 +0200"#;
+        let regex = Regex::new(super::UUID_REGEX).unwrap();
+        let captures = regex.captures(&line).unwrap();
+
+        assert_eq!(
+            captures.get(1).unwrap().as_str(),
+            "df7f9091-18d5-4002-91c9-e084516526ab"
+        );
+    }
+
+    #[test]
+    fn regex_matches_uuid_on_line_with_brackets() {
+        let line = r#"[be155bd9-587d-468a-994f-441815edc79d]   CACHE MyModel Load (0.0ms)  SELECT  `my_models`.* FROM `my_models` WHERE `my_models`.`id` = 1 LIMIT 1  [["id", 1], ["LIMIT", 1]]"#;
+        let regex = Regex::new(super::UUID_REGEX).unwrap();
+        let captures = regex.captures(&line).unwrap();
+
+        assert_eq!(
+            captures.get(1).unwrap().as_str(),
+            "be155bd9-587d-468a-994f-441815edc79d"
+        );
     }
 }
