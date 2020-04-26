05a53a5db075cd29828a43dff9beffec91b91b02
Fran Casas <nflamel@gmail.com>
2020-04-26 21:35:02 +0200

    feat: add new lines into a log

--- src/log.rs
+++ src/log.rs
@@ -10,12 +10,16 @@ struct Log<'a> {
     entries: Vec<&'a str>,
 }
 
-impl<'a> Log<'_> {
+impl<'a> Log<'a> {
     fn new() -> Self {
         Log {
             state: State::Unknown,
             entries: Vec::new(),
         }
+    }
+
+    fn add(&mut self, line: &'a str) {
+        self.entries.push(line);
     }
 }
 
@@ -24,10 +28,21 @@ mod tests {
     use super::*;
 
     #[test]
-    fn new_log_entry() {
+    fn test_new() {
         let log = Log::new();
 
         assert_eq!(log.state, State::Unknown);
         assert!(log.entries.is_empty())
+    }
+
+    #[test]
+    fn test_add_line() {
+        let mut log = Log::new();
+        let line = "A Line";
+
+        log.add(line);
+
+        assert_eq!(log.entries.len(), 1);
+        assert_eq!(log.entries.pop(), Some("A Line"));
     }
 }
