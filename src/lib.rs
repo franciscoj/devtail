805b161655bc7a46efe467b3c874320ab1f1d444
Fran Casas <nflamel@gmail.com>
2020-04-28 21:33:53 +0200

    refactor: extract HTTP status

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
 pub mod tail;
 pub mod log;
 pub mod parser;
+
+#[derive(Debug, PartialEq)]
+pub enum HttpStatus {
+    Success(u16),
+    Redirect(u16),
+    ClientError(u16),
+    ServerError(u16),
+    Unknown(u16)
+}
+
