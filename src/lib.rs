5795e95a85dd725ec5fd06b9fa09736b58706335
Fran Casas <nflamel@gmail.com>
2020-04-29 22:01:31 +0200

    doc: HttpStatus

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,20 @@ pub mod parser;
 pub mod entry;
 
 #[derive(Debug, PartialEq)]
+// TODO: Map HTTP states from 100..=199
 pub enum HttpStatus {
+    /// HTTP States from `200..=299`
     Success(u16),
+    /// HTTP States from `300..=399`
     Redirect(u16),
+    /// HTTP States from `400..=499`
     ClientError(u16),
+    /// HTTP States from `500..=599`
     ServerError(u16),
+    /// To have the same "ducktype" as the rest of variants it has an u16 attached but it is set to
+    /// 0 by default.
+    ///
+    /// In case it receives an error < 200 or > 600 the value comes here.
     Unknown(u16)
 }
 
