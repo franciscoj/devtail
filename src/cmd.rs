008abb2682bd9cb06c81dd7097a1ee44ed3ba160
Fran Casas <nflamel@gmail.com>
2020-05-01 20:49:07 +0200

    feat: print with termion
    Clear the screen before printing and start by the top of the screen.

--- src/cmd.rs
+++ src/cmd.rs
@@ -1,12 +1,17 @@
+extern crate termion;
+
 use super::log::Log;
 use super::tail::Tail;
 use std::io::BufRead;
+use termion::{clear, cursor};
 
 /// Runs a `Tail`.
 ///
 /// Running a `Tail` means iterating on each line coming from the source (either a file or stdin)
 /// and printing them.
 pub fn run<T: BufRead>(tail: Tail<T>) {
+    println!("{}{}", clear::All, cursor::Goto(1, 1));
+
     let mut log = Log::new();
 
     for line in tail {
