79e5f3148bd38d3c728c4decc2c7310b897abce2
Fran Casas <nflamel@gmail.com>
2020-05-02 21:20:07 +0200

    feat: calculate screen line for a line in the log

--- src/screen.rs
+++ src/screen.rs
@@ -13,14 +13,20 @@ impl Screen {
         Self { size }
     }
 
-    fn line_for(&self, log: &Log, id: String) -> Option<u16> {
+    fn line_nr_for(&self, log: &Log, id: String) -> Option<u16> {
         let entry = log.get(id)?;
 
-        // let size = log.len();
-        // let (_, rows) = self.size;
+        let log_size = u16::try_from(log.len()).unwrap();
+        let (_, rows) = self.size;
         let order = u16::try_from(entry.order).unwrap();
 
-        Some(order)
+        if log_size <= rows {
+            Some(order)
+        } else if order > log_size - rows {
+            Some(order + rows - log_size)
+        } else {
+            None
+        }
     }
 }
 
@@ -30,7 +36,7 @@ mod tests {
     use super::Screen;
 
     #[test]
-    fn test_line_for_when_all_fit() {
+    fn test_line_nr_for_when_all_fit() {
         let mut log = Log::new();
         let uuid0 = log.add(log_start!("0")).unwrap();
         let uuid1 = log.add(log_start!("1")).unwrap();
@@ -38,7 +44,46 @@ mod tests {
 
         let screen = Screen::new(size);
 
-        assert_eq!(screen.line_for(&log, uuid0), Some(1));
-        assert_eq!(screen.line_for(&log, uuid1), Some(2));
+        assert_eq!(screen.line_nr_for(&log, uuid0), Some(1));
+        assert_eq!(screen.line_nr_for(&log, uuid1), Some(2));
+    }
+
+    #[test]
+    fn test_line_nr_for_when_lines_out_of_screen() {
+        let mut log = Log::new();
+        let uuids = vec![
+            log.add(log_start!("0")).unwrap(),
+            log.add(log_start!("1")).unwrap(),
+            log.add(log_start!("2")).unwrap(),
+        ];
+
+        let size = (1, 2);
+
+        let screen = Screen::new(size);
+
+        assert_eq!(screen.line_nr_for(&log, uuids[0].clone()), None);
+        assert_eq!(screen.line_nr_for(&log, uuids[1].clone()), Some(1));
+        assert_eq!(screen.line_nr_for(&log, uuids[2].clone()), Some(2));
+    }
+
+    #[test]
+    fn test_line_nr_for_on_many_lines() {
+        let mut log = Log::new();
+        let uuids = vec![
+            log.add(log_start!("0")).unwrap(),
+            log.add(log_start!("1")).unwrap(),
+            log.add(log_start!("2")).unwrap(),
+            log.add(log_start!("3")).unwrap(),
+            log.add(log_start!("4")).unwrap(),
+            log.add(log_start!("5")).unwrap(),
+            log.add(log_start!("6")).unwrap(),
+            log.add(log_start!("7")).unwrap(),
+        ];
+        let size = (1, 2);
+
+        let screen = Screen::new(size);
+
+        assert_eq!(screen.line_nr_for(&log, uuids[3].clone()), None);
+        assert_eq!(screen.line_nr_for(&log, uuids[7].clone()), Some(2));
     }
 }
